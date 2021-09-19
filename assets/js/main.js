43ff13a94c4162e851129bd913e887c112dd3f92
iko1209 <90879446+iko1209@users.noreply.github.com>
2021-09-19 12:33:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: 'タイタニック',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'OPEN',
           // q2: 'えええ',
           // q3: 'おおお'
         },
